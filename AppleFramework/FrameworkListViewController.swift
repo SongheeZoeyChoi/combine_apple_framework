7380603f8c50b05cd03346a8c5d12e32470cb696
songhee <csh0801@dholic.com>
2022-07-19 14:06:28 +0900

    Refactoring Combine (List)

--- AppleFramework/FrameworkListViewController.swift
+++ AppleFramework/FrameworkListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class FrameworkListViewController: UIViewController {
     
@@ -18,12 +19,64 @@ class FrameworkListViewController: UIViewController {
     
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
-    let list: [AppleFramework] = AppleFramework.list
+//    let list: [AppleFramework] = AppleFramework.list
+    
+    //Combine
+    let didSelect = PassthroughSubject<AppleFramework, Never>()
+    var subscription = Set<AnyCancellable>()
+//    @Published var list: [AppleFramework] = AppleFramework.list
+    let items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)
     
     // Data, Presentation, Layout
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // CollectionView (Presentation, Layout) 정보 설정 해주는 것
+        // CollectionView (Data) 그리는데 필요한 데이터 설정해주는 것
+        configureCollectionView()
+        bind()
+        
+        
+//        // data
+//        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+//        snapshot.appendSections([.main])
+//        snapshot.appendItems(list, toSection: .main)
+//        dataSource.apply(snapshot)
+        
+        // layer
+        collectionView.collectionViewLayout = layout()
+        
+        collectionView.delegate = self
+    }
+    private func bind() { // 핵심 로직 넣어 둠 (input, output)
+        // input : 사용자 인풋을 받아서, 처리해야 할 것 //
+        // - item 선택 되었을 때 처리
+        didSelect
+            .receive(on: RunLoop.main)  // 아래 UI변경이니까
+            .sink { [unowned self] framework in
+                let sb = UIStoryboard(name: "Detail", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+                vc.framework = framework
+                
+                self.present(vc, animated: true)
+            }.store(in: &subscription)
+        
+        // output : data, state 변경에 따라서, UI업데이트 할 것 //
+        // - items가 세팅이 되었을 때, 컬렉션뷰를 업데이트
+        items
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] list in
+                self.applySectionItems(list)
+            }.store(in: &subscription)
+    }
+    
+    private func applySectionItems(_ items: [Item], to section: Section = .main) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([section])
+        snapshot.appendItems(items, toSection: section)
+        dataSource.apply(snapshot)
+    }
+    private func configureCollectionView() {
         // presentation
         dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
@@ -32,17 +85,6 @@ class FrameworkListViewController: UIViewController {
             cell.configure(item)
             return cell
         })
-        
-        // data
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(list, toSection: .main)
-        dataSource.apply(snapshot)
-        
-        // layer
-        collectionView.collectionViewLayout = layout()
-        
-        collectionView.delegate = self
     }
     
     private func layout() -> UICollectionViewCompositionalLayout {
@@ -67,13 +109,16 @@ class FrameworkListViewController: UIViewController {
 
 extension FrameworkListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let framework = list[indexPath.item]
+//        let framework = list[indexPath.item]
+        let framework = items.value[indexPath.item]
         print(">>> selected: \(framework.name)")
         
-        let sb = UIStoryboard(name: "Detail", bundle: nil)
-        let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
-        vc.framework = framework
+        didSelect.send(framework)
         
-        present(vc, animated: true)
+//        let sb = UIStoryboard(name: "Detail", bundle: nil)
+//        let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+//        vc.framework = framework
+//
+//        present(vc, animated: true)
     }
 }
